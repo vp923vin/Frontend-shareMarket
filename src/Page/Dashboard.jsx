5ed4c8f32d4341478b8d98afceb425f35d9e5ffc
Vipin Patel <vp923vin@gmail.com>
2024-04-20 19:22:29 +0530

    adding new dashboard

--- src/Page/Dashboard.jsx
+++ src/Page/Dashboard.jsx
@@ -1,56 +1,112 @@
-import React from 'react'
-import '../Page/Dashboard.css'
-import TradingViewChart from '../components/Chartview'
-import Header from '../components/Header'
-import Progressbar from '../components/Progressbar'
-import { useContextApi } from '../components/context/UseContext'
+import React from "react";
+import "../Page/Dashboard.css";
+import TradingViewChart from "../components/Chartview";
+import Header from "../components/Header";
+import Progressbar from "../components/Progressbar";
+import { useContextApi } from "../components/context/UseContext";
 
 function Dashboard() {
-    const { theme } = useContextApi()
-    return (
+  const { theme } = useContextApi();
+  return (
+    <>
+      <div className="  dark:text-white dark:bg-[#212529] h-screen ">
+        <Header />
 
-        <>
-
-            <Header />
-
-            <section className={`first_dash_page ${theme}`}>
-                <div className="container_fluid">
-                    <div className="row m-0">
-                        <div className="col-6">
-                            <TradingViewChart />
-                        </div>
-                        <div className="col-6 sec_col">
-                            <div className="row m-0">
-                                <div className="col-5">
-                                    <img className="candle_img" src="./images/chart-candle.png" alt="" />
-                                </div>
-                                <div className="col-7 news">
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
-                                    <p>Velit quis enim omnis ut eaque veniam illum doloribus obcaecati saepe culpa?</p>
-                                    <p>Velit quis enim omnis ut eaque veniam illum doloribus obcaecati saepe culpa?</p>
-                                    <p>Velit quis enim omnis ut eaque veniam illum doloribus obcaecati saepe culpa?</p>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                    <div className="row m-0 py-4">
-                        <div className="col-6">
-                            <img className="img-fluid" src="./images/tradchart_img.png" alt="" />
-                        </div>
-                        <div className="col-6">
-                            <Progressbar />
-
-                        </div>
-                    </div>
+        {/* <section className={`first_dash_page ${theme}`}>
+        <div className="container_fluid">
+          <div className="row m-0">
+            <div className="col-6">
+              <TradingViewChart />
+            </div>
+            <div className="col-6 sec_col">
+              <div className="row m-0">
+                <div className="col-5">
+                  <img
+                    className="candle_img"
+                    src="./images/chart-candle.png"
+                    alt=""
+                  />
                 </div>
-            </section>
+                <div className="col-7 news">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.{" "}
+                  </p>
+                  <p>
+                    Velit quis enim omnis ut eaque veniam illum doloribus
+                    obcaecati saepe culpa?
+                  </p>
+                  <p>
+                    Velit quis enim omnis ut eaque veniam illum doloribus
+                    obcaecati saepe culpa?
+                  </p>
+                  <p>
+                    Velit quis enim omnis ut eaque veniam illum doloribus
+                    obcaecati saepe culpa?
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row m-0 py-4">
+            <div className="col-6">
+              <img
+                className="img-fluid"
+                src="./images/tradchart_img.png"
+                alt=""
+              />
+            </div>
+            <div className="col-6">
+              <Progressbar />
+            </div>
+          </div>
+        </div>
+      </section> */}
 
+        <section className="container py-1 ">
+          <div className="grid grid-cols-1 md:grid-cols-2 md:gap-12 ">
+            <div>
+              <TradingViewChart />
+            </div>
+            <div>
+              <div className="flex gap-8 ">
+                <div className="">
+                  <img
+                    className="candle_img"
+                    src="./images/chart-candle.png"
+                    alt=""
+                  />
+                </div>
+                <div className="border-2 border-red-600 dark:border-blue-600  p-4 dark:text-white">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.{" "}
+                  </p>
+                  <p>
+                    Velit quis enim omnis ut eaque veniam illum doloribus
+                    obcaecati saepe culpa?
+                  </p>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
 
-
-
-        </>
-    )
+          <div className="grid grid-cols-1 lg:grid-cols-2 mt-2 md:gap-12 ">
+            <div className="">
+              {/* <img
+                className="img-fluid"
+                src="./images/tradchart_img.png"
+                alt=""
+              /> */}
+                <TradingViewChart />
+            </div>
+            <div className="">
+              <Progressbar />
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
--- src/components/Progressbar.jsx
+++ src/components/Progressbar.jsx
@@ -1,43 +1,95 @@
-import React from 'react'
-import '../components/progressbar.css'
+import React from "react";
+import "../components/progressbar.css";
 
 function Progressbar() {
   return (
+    // <div className="progresbar_div">
+    //   <div className="progress-container">
 
-    <div className="progresbar_div">
-      <div className="progress-container">
+    //     <div className="progress-bar">
+    //       <div className="progress-fill" style={{ width: '50%' }}>Progress 1</div>
+    //     </div>
 
-        <div className="progress-bar">
-          <div className="progress-fill" style={{ width: '50%' }}>Progress 1</div>
+    //     <div className="progress-bar">
+    //       <div className="progress-fill" style={{ width: '60%' }}>Progress 2</div>
+    //     </div>
+
+    //     <div className="progress-bar">
+    //       <div className="progress-fill" style={{ width: '80%' }}>Progress 3</div>
+    //     </div>
+
+    //     <div className="progress-bar">
+    //       <div className="progress-fill" style={{ width: '70%' }}>Progress 4</div>
+    //     </div>
+
+    //     <div className="progress-bar">
+    //       <div className="progress-fill" style={{ width: '90%' }}>Progress 4</div>
+    //     </div>
+
+    //     <div className="progress-bar">
+    //       <div className="progress-fill" style={{ width: '100%' }}>Progress 4</div>
+    //     </div>
+
+    //     <div className="progress-bar">
+    //       <div className="progress-fill" style={{ width: '90%' }}>Progress 4</div>
+    //     </div>
+
+    //   </div>
+    // </div>
+
+    <>
+      <div>
+        <div className="w-full bg-gray-200 h-6  mb-3 dark:bg-gray-700">
+          <div
+            className="bg-blue-600 h-6 text-white flex items-center justify-center"
+            style={{ width: "80%" }}
+          >
+            85%
+          </div>
         </div>
-
-        <div className="progress-bar">
-          <div className="progress-fill" style={{ width: '60%' }}>Progress 2</div>
+        <div className="w-full bg-gray-200 h-6  mb-3 dark:bg-gray-700">
+          <div
+            className="bg-red-600 h-6 text-white flex items-center justify-center"
+            style={{ width: " 80%" }}
+          >
+          80%
+          </div>
         </div>
-
-        <div className="progress-bar">
-          <div className="progress-fill" style={{ width: '80%' }}>Progress 3</div>
+        <div className="w-full bg-gray-200 h-6  mb-3 dark:bg-gray-700">
+          <div
+            className="bg-green-600 h-6 text-white flex items-center justify-center"
+            style={{ width: "70%" }}
+          >
+             70%
+          </div>
         </div>
-
-        <div className="progress-bar">
-          <div className="progress-fill" style={{ width: '70%' }}>Progress 4</div>
+        <div className="w-full bg-gray-200 h-6  mb-3 dark:bg-gray-700">
+          <div
+            className="bg-yellow-400 h-6 flex items-center justify-center"
+            style={{ width: "50%" }}
+          >
+            50%
+          </div>
         </div>
-
-        <div className="progress-bar">
-          <div className="progress-fill" style={{ width: '90%' }}>Progress 4</div>
+        <div className="w-full bg-gray-200 h-6  mb-3 dark:bg-gray-700">
+          <div
+            className="bg-indigo-600 h-6 text-white flex items-center justify-center"
+            style={{ width: "45%" }}
+          >
+            45%
+          </div>
         </div>
-
-        <div className="progress-bar">
-          <div className="progress-fill" style={{ width: '100%' }}>Progress 4</div>
+        <div className="w-full bg-gray-200 h-6 dark:bg-gray-700">
+          <div
+            className="bg-purple-600 h-6 text-white flex items-center justify-center"
+            style={{ width: "45%" }}
+          >
+            45%
+          </div>
         </div>
-
-        <div className="progress-bar">
-          <div className="progress-fill" style={{ width: '90%' }}>Progress 4</div>
-        </div>
-
       </div>
-    </div>
-  )
+    </>
+  );
 }
 
-export default Progressbar
+export default Progressbar;
